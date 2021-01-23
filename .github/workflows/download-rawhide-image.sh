6854b7d871f67e4e107a37d75a965083d153ed81
Ondrej Mosnacek <omosnace@redhat.com>
2021-01-23 12:11:15 +0100

    fixes

--- .github/workflows/download-rawhide-image.sh
+++ .github/workflows/download-rawhide-image.sh
@@ -8,7 +8,8 @@ BASE_URL="https://download.fedoraproject.org/pub/fedora/linux/development/rawhid
 GPG_URL="https://getfedora.org/static/fedora.gpg"
 IMG_SUFFIX=".vagrant-virtualbox.box"
 
-wget -r -nd -np -l 1 -H -e robots=off -A "*$IMG_SUFFIX,*-CHECKSUM" "$BASE_URL"
+wget --progress dot:mega -r -nd -np -l 1 -H -e robots=off \
+    -A "*$IMG_SUFFIX,*-CHECKSUM" "$BASE_URL"
 
 latest_image="$(ls -1q *$IMG_SUFFIX | tail -n 1)"
 if [ -z "$latest_image" ]; then
@@ -18,4 +19,4 @@ fi
 
 shasum -a 256 --status --ignore-missing -c ./*-CHECKSUM
 
-echo "$(readlink -f "$latest_image")"
+echo "$(realpath "$latest_image")"
